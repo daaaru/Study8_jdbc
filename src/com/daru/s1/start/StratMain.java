0987fed8479258c399323e7b43bc61a47ed84662
Jeonjueun07 <lovesj2323@naver.com>
2022-01-19 12:36:30 +0900

    add_2_main

---
+++ src/com/daru/s1/start/StratMain.java
@@ -0,0 +1,12 @@
+package com.daru.s1.start;
+
+public class StratMain {
+
+	public static void main(String[] args) {
+		System.out.println("DB 연동 테스트시작");
+		
+		
+		System.out.println("DB 연동 테스트 끝");
+	}
+
+}
